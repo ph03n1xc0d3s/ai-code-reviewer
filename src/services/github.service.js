fa7a4468775b892f254b39d0bc03c98dd4c97d9b
Vaibhav Maini <ph03n1xc0d3s@gmail.com>
2026-03-30 12:45:58 +0530

    removed console.log statement from github service module

--- src/services/github.service.js
+++ src/services/github.service.js
@@ -31,10 +31,8 @@ export async function handlePR(payload) {
   console.log("Diff fetched");
 
   // 🔥 run your analyzer
-  console.log("Starting analysis...");
-  console.log(diff, "diff to analyze");
   const result = await processReview(diff);
-  
+
   console.log("Analysis complete:", result.totalIssues);
 
   // 🔥 post result
